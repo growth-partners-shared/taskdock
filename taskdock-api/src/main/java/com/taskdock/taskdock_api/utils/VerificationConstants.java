355444138045199de4a4a95beec993414edca0b1
Sri Venkata Ganesh Teja Potti <ganesh.potti27@gmail.com>
2026-07-26 14:55:09 +0530

    add: app & verification constants

--- taskdock-api/src/main/java/com/taskdock/taskdock_api/utils/VerificationConstants.java
+++ taskdock-api/src/main/java/com/taskdock/taskdock_api/utils/VerificationConstants.java
@@ -7,4 +7,8 @@ public final class VerificationConstants {
   private VerificationConstants() {}
 
   public static final Duration EMAIL_OTP_EXPIRY = Duration.ofMinutes(10);
+
+  public static final Duration RESET_PASSWORD_OTP_EXPIRY = Duration.ofMinutes(10);
+
+  public static final Duration RESET_PASSWORD_TOKEN_EXPIRY = Duration.ofMinutes(10);
 }
