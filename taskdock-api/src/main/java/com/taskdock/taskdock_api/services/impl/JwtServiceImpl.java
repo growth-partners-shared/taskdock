8677c863497a58364f36e22da0a52fb9d0e144c3
Sri Venkata Ganesh Teja Potti <ganesh.potti27@gmail.com>
2026-08-02 17:03:20 +0530

    fix: user + notification

--- taskdock-api/src/main/java/com/taskdock/taskdock_api/services/impl/JwtServiceImpl.java
+++ taskdock-api/src/main/java/com/taskdock/taskdock_api/services/impl/JwtServiceImpl.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class JwtServiceImpl implements JwtService {
 
-  @Value("${jwt.secret-key}")
+  @Value("${jwt.secret-key:${JWT_SECRET}}")
   private String jwtSecretKey;
 
   private SecretKey getSecretKey() {
